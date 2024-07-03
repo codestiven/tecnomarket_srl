c12bb7c52310ce76b0586d5e95cae6f6720cff69
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-07-02 23:02:50 -0400

    flowbite

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        
 
         <!-- Scripts -->
         @routes
@@ -17,5 +18,13 @@
     </head>
     <body class="font-sans antialiased">
         @inertia
+
+
+
+
+        <script src="{{ asset('path/to/flowbite/dist/flowbite.min.js') }}"></script>
+<link href="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.js"></script>
+
     </body>
 </html>
