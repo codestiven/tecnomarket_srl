e49818014f2af50291d4b8c172c8cfbcedebf558
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-07-02 23:03:44 -0400

    componentes importantes

---
+++ resources/js/Components/Boton_principar.vue
@@ -0,0 +1,29 @@
+<template>
+    <button class="">
+        <slot />
+    </button>
+</template>
+
+<style scoped>
+button {
+    background-color: var(--secondary-color);
+    padding: 10px;
+    border-radius: 10px;
+    color: white;
+    font-weight: bold;
+    font-size: 20px;
+    border: none;
+    cursor: pointer;
+    transition: all 0.3s ease;
+}
+
+button:hover{
+    transform: scale(1.3);
+}
+
+button:active{
+    transform: scale(0.9);
+}
+
+</style>
+
---
+++ resources/js/Components/Titulos.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="elemento flex flex-col items-center">
+        <h1 class="mb-4">{{ titulo }}</h1>
+        <h2 v-if="subtitulo">{{ subtitulo }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        titulo: {
+            type: String,
+            required: true
+        },
+        subtitulo: {
+            type: String,
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+  .elemento {
+      margin: 100px 0px 100px 0px;
+  }
+
+  h1 {
+    font-size: 48px;
+    color: #2B2B2B;
+    font-weight: bold;
+
+
+  }
+
+  h2 {
+    font-size: 24px;
+    color: #2b2b2bab;
+
+
+  }
+</style>
