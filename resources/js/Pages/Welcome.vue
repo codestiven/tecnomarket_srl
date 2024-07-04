c1e5e39772196c94ae3cb2602bee379536ac33c1
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-07-04 18:41:32 -0400

    quienes somos agregacion de local de flowbite y otros cambios

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -5,6 +5,7 @@ import Footer from "@/Components/Principales/Footer.vue";
 import Titulo from "@/Components/Titulos.vue";
 import Carruser from "@/Components/Unicos/Carrusel.vue";
 import boton1 from "@/Components/Boton_principar.vue";
+import Mapa from "@/Components/mapa.vue";
 
 import Destacados from "@/Components/Unicos/Destacados.vue";
 import Ofertas from "@/Components/Unicos/Ofertas.vue";
@@ -142,16 +143,7 @@ function handleImageError() {
 
   <Titulo titulo="Donde nos encontramos"      :mostrarLinea="false"/>
 
-  <iframe
-    class="mapa"
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3785.3726697721395!2d-70.10231242505938!3d18.421380972197042!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ea55f95af918ad5%3A0x17aced50d30659a7!2sTECNOMARKET%20SRL!5e0!3m2!1ses-419!2sdo!4v1719876703382!5m2!1ses-419!2sdo"
-    width="600"
-    height="450"
-    style="border: 0"
-    allowfullscreen=""
-    loading="lazy"
-    referrerpolicy="no-referrer-when-downgrade"
-  ></iframe>
+<Mapa/>
 
   <!-- -----------------------------------------------------  Footer ------------------------------------ -->
 
@@ -348,12 +340,7 @@ function handleImageError() {
   text-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
 }
 
-/* mapa -------------- ----------------------------------------- */
-.mapa {
-  width: 100%;
 
-  margin: 120px 0px 120px 0px;
-}
 
 
 
