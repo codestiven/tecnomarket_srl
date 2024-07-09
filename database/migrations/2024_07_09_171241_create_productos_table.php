b47ea37beab956ceb266cb3c2e522aa16b547dc6
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-07-09 13:08:34 -0400

    Migraciones( producto , caregorias , marcas , ofertas )

---
+++ database/migrations/2024_07_09_171241_create_productos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->text('descripción')->nullable();
+            $table->decimal('precio', 10, 2);
+            $table->unsignedBigInteger('categoria_id');
+            $table->unsignedBigInteger('marca_id');
+            $table->unsignedBigInteger('oferta_id')->nullable();
+            $table->string('image')->nullable();
+            $table->timestamps();
+
+            // Claves foráneas
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+            $table->foreign('marca_id')->references('id')->on('marcas');
+            $table->foreign('oferta_id')->references('id')->on('ofertas')->onDelete('set null');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
