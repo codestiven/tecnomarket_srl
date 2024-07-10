68a6df19bc0fb150adfaa2c34bbbefedd8aa3405
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-07-10 15:15:29 -0400

    que el botón dirija a los datos espesificos del producto no es necesario diseno y buscar

--- resources/js/Components/buscador.vue
+++ resources/js/Components/buscador.vue
@@ -1,11 +1,9 @@
 <template>
     <div class="buscador">
-        <input type="text" class="input-buscador" placeholder="Buscar...">
-        <button>
+        <input type="text" class="input-buscador" v-model="busqueda" placeholder="Buscar...">
+        <button @click="buscarProducto">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-icon">
-                <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-                <path
-                    d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
+                <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
             </svg>
         </button>
     </div>
@@ -13,7 +11,23 @@
 
 <script>
 export default {
-    // Componente Vue
+    data() {
+        return {
+            busqueda: ''
+        };
+    },
+    methods: {
+        buscarProducto() {
+            // Redirigir usando Inertia.js
+            if (this.busqueda === '') {
+                this.$inertia.visit('/Productos/search/*');
+                
+            } else {
+                this.$inertia.visit(`/Productos/search/${this.busqueda}`);
+            }
+
+        }
+    }
 }
 </script>
 
