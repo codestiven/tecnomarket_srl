6bd256916320068a835c85cababbd39f80d063ad
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-08-12 14:59:07 -0400

    Error productos

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -22,36 +22,8 @@ class ProductoController extends Controller
             $producto->image = Storage::url($producto->image);
         }
 
-
         // Devolver la respuesta JSON con todos los productos actualizados
         return response()->json(['productos' => $productos]);
-        
-    }
-
-
-
-        // Devolver la respuesta JSON con todos los productos actualizados
-        return response()->json(['productos' => $productos]);
-
-
-    public function search(Request $request, $buscar = null)
-    {
-        if ($buscar != "*") {
-            $resultados = Producto::where('nombre', 'like', '%' . $buscar . '%')->get();
-        } else {
-            $resultados = Producto::inRandomOrder()->get();
-        }
-
-        foreach ($resultados as $resultado) {
-            $resultado->image = Storage::url($resultado->image);
-        }
-
-        return Inertia::render('Productos/Buscar', [
-            'productos' => $resultados
-        ]);
-    }
-
-
 
         
         
@@ -107,7 +79,6 @@ class ProductoController extends Controller
             'productos' => $resultados
         ]);
     }
-
 
 
     /**
