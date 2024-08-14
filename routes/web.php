2f53c8bc89122fab488c06a4824c7006932f02ad
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-08-14 17:10:41 -0400

    pequeno error con web

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\GuardadoController;
-use App\Http\Controllers\GuardadoController;
+
 
 // Controladores
 use App\Http\Controllers\ProfileController;
