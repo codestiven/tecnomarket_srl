bd299127041fa28a4508c6219413c0201664710b
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-08-30 10:05:04 -0400

    arreglo temportar de boton

--- resources/js/Components/Principales/Header.vue
+++ resources/js/Components/Principales/Header.vue
@@ -207,7 +207,7 @@ onMounted(() => {
           <div class="text-center"></div>
           <div>
             <div v-if="isMobile" class="flex items-center justify-center">
-              <button
+              <!-- <button
                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                 type="button" data-drawer-target="drawer-example" data-drawer-show="drawer-example"
                 aria-controls="drawer-example">
@@ -216,6 +216,12 @@ onMounted(() => {
                   <path fill="#ffffff"
                     d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
                 </svg>
+              </button> -->
+
+              <button
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                type="button">
+                <Link :href="route('Productos')"> Productos </Link>
               </button>
             </div>
             <div v-else>
@@ -242,7 +248,7 @@ onMounted(() => {
           </div>
         </div>
       </div>
-      
+
 
       <div class="seccion">
         <div v-if="$page.props.auth.user" class="relative inline-block text-left">
