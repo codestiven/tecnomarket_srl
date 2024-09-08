1bed173b15e87b20b5f875e94c4ec4e3199c8592
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-09-07 20:58:42 -0400

    reordenamiento de api

--- routes/api.php
+++ routes/api.php
@@ -5,29 +5,29 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\MarcaController;
-use App\Http\Controllers\OfertaController;
+
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\GuardadoController;
 use App\Http\Controllers\CarouselController;
 use App\Http\Controllers\ProvinciaController;
+use App\Http\Controllers\SuggestionController;
 
 
 
 
 
 
-
-
-
+// Rutas de Productos
 Route::get('/ProductoCount', [ProductoController::class, 'ContadorProductos']);
+Route::get('/productosAll', [ProductoController::class, 'index']);
+Route::get('/productos/filtrar', [ProductoController::class, 'filtrarProductos']);
+Route::get('productos/ofertas', [ProductoController::class, 'obtenerProductosEnOferta']);
+Route::post('/productos/por-ids', [ProductoController::class, 'getProductsByIds']);
 
-
+// Rutas de Productos Guardados (Liked)
 Route::get('/Productoliked', [GuardadoController::class, 'getGuardados']);
 
-
-// Route::get('/Marcas', [MarcaController::class, 'index']);
-
-
+// Rutas de Categorías
 Route::prefix('Categorias')->group(function () {
     Route::get('/', [CategoriaController::class, 'index']);
     Route::post('/', [CategoriaController::class, 'store']);
@@ -36,28 +36,20 @@ Route::prefix('Categorias')->group(function () {
     Route::delete('/{categoria}', [CategoriaController::class, 'destroy']);
 });
 
-
-
-
-Route::get('/productos/filtrar', [ProductoController::class, 'filtrarProductos']);
-Route::get('/productosAll', [ProductoController::class, 'index']);
-Route::post('/productos/por-ids', [ProductoController::class, 'getProductsByIds']);
-
-
-Route::get('productos/ofertas', [ProductoController::class, 'obtenerProductosEnOferta']);
-
+// Rutas de Marcas
 Route::prefix('Marcas')->group(function () {
     Route::get('/', [MarcaController::class, 'index']);
     Route::get('/create', [MarcaController::class, 'create']);
     Route::get('/{marca}', [MarcaController::class, 'show']);
     Route::get('/{marca}/edit', [MarcaController::class, 'edit']);
-
 });
 
+// Rutas de Sugerencias
+Route::get('/suggestions', [SuggestionController::class, 'index']);
 
+// Rutas de Carruseles
 Route::get('/carousels', [CarouselController::class, 'index']);
 Route::get('/carousels/{id}', [CarouselController::class, 'show']);
 
+// Rutas de Provincias
 Route::get('/province', [ProvinciaController::class, 'index']);
-
-
